974a5cc829dc231d9292b90b37bab154ae4661cd
ANDRII SYNII <79702542+Angri154@users.noreply.github.com>
2023-03-23 18:19:35 +0200

    as

---
+++ index.a253a019.js
@@ -0,0 +1,2 @@
+const form=document.querySelector(".form"),input=document.querySelector(".form-input");input.addEventListener("focus",(()=>{form.classList.add("form--active")})),input.addEventListener("blur",(()=>{form.classList.remove("form--active")}));
+//# sourceMappingURL=index.a253a019.js.map
